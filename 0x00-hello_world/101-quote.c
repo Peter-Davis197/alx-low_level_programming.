7167198daa0050469e08a1ffd315da0b58e81860
peter-davis197 <noblerexpeterdavis197@gmail.com>
2022-10-13 16:13:46 -0700

    Hello C world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
- * Return: 1
+ * main - Prints text
+ *Return: 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19);
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 /**
  *
- *  * main - Entry point
+ *main - Points the size of computer types
  *
- *   *
- *
- *    * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  *
  */
 
 int main(void)
 
 {
+	char a;
+	int b;
+	lon int c;
+	long long int d;
+	float e;
+
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a lond int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", Sizeof(float));
 
