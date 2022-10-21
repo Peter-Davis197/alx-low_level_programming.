753eaba667a8b59eb306783e18ffc7a0192fc08d
peter-davis197 <noblerexpeterdavis197@gmail.com>
2022-10-20 17:45:18 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a = o;
+
+	while (a < 10)
+		-putchar(a++ + '0');
+
+	_putchar('\n');
+}
