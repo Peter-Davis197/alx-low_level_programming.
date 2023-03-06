d1dfade20579df3ef2a09b18bef6fdd407f7e086
peter-davis197 <noblerexpeterdavis197@gmail.com>
2023-03-06 15:25:14 -0800

    hope

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,20 +3,20 @@
 /**
  * _memcpy - copy memory area
  * @dest: dest memory area
- * @src: source memiry area
+ * @src: source memory area
  * @n: bytes from memory area to copy
  *
- * Return: memory area replaced
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int a;
+	unsigned int i;
 
-	for (a = 0; a < n; a++)
+	for (i = 0; n > 0; i++, n--)
 
 	{
-		dest[a] = src[a];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
