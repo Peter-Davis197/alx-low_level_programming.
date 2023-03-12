31dd7f5658669d11acda8833ca6a246252b8896d
peter-davis197 <noblerexpeterdavis197@gmail.com>
2023-03-11 21:13:07 -0800

    c- Static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * _puts - characters in puts
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
