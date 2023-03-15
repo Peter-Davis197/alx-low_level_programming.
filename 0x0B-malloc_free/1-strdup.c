9e379e7f4b942499b2346dbd4abaac7e11fc9929
peter-davis197 <noblerexpeterdavis197@gmail.com>
2023-03-14 19:57:57 -0700

    corrections on malloc free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,56 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-
 /**
- * _strlen - count array
- * @s: array of elements
- * Return: 1
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
  */
 
-int _strlen(char *s)
+char *_strdup(char *str)
 {
-	unsigned int i;
+	char *aaa;
+	int i, r = 0;
 
-	while (s[i] != '\0') /*count character of string*/
-	{
-		i++;
-	}
-
-	return (1);
-}
-
-/**
- * _strcpy - copy arrays
- * @src: arry of elements
- * @dest: dest arry
- * Return: dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
-
-/**
- * _strdup - array for prints a strin
- * @str: array of elements
- * Return: pointer
- */
-
-char *_strdup(char *str);
-{
-	char *dst;
-	unsigned int size;
-
-	if (str == 0)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	aaa = malloc(sizeof(char) * (i + 1));
+
+	if (aaa == NULL)
+		return (NULL);
+
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
+
+	return (aaa);
+}
