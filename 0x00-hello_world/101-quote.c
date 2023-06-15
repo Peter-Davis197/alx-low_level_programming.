1162a98a25de49dbce2f310d57427aa8175ad044
Peter M. Davis <113861020+Peter-Davis197@users.noreply.github.com>
2023-06-15 09:35:27 +0100

    Update 101-quote.c correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
+
 /**
- * main - Prints text
+ * main - Prints a message to the standard error
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19);
-	return (1);
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    int length = 55;
+
+    write(2, message, length);
+    return 1;
 }
